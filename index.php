74d6bcd2258f946646127a220a2e301adad28267
Midas <eyeofmidas@gmail.com>
2015-02-28 17:14:08 -0500

    adding authentication notes to help file

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 if(isset($_REQUEST['help'])) { ?>
 <p>To use:</p>
 <ul>
+	<li><strong>LOG IN</strong> - <a href="/?username=admin&password=password">/?username=[some username]&password=[some password]</a></li>
+	<li><strong>LOG OUT</strong> - <a href="/?logout">/?logout</a></li>
 	<li><strong>GET</strong> - <a href="/?method=get">/?method=GET</a></li>
 	<li><strong>GET</strong> - <a href="/?method=get&key=key">/?method=GET&key=[some key]</a></li>
 	<li><strong>POST</strong> - <a href="/?method=post&key=key&value=value">/?method=POST&key=[some key]&value=[some value]</a></li>
